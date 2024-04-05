e1f6ac83d9b1be2b19294686b3780ac0abe3d65a
Gallifrey23 <162268552+Gallifrey23@users.noreply.github.com>
2024-04-05 15:07:58 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TW'
+copyright = '2024,Gallifrey23 '
+author = 'Gallifrey23'
 
 release = '0.1'
 version = '0.1.0'
